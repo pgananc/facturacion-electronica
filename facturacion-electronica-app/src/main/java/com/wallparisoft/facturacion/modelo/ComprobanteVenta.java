7afedc42f3b0ecf4f2563defd08f8f33ee7c7822
jeddiewars <jguerreroh@outlook.com>
2020-10-17 13:56:54 -0500

    se agrega entidad ComprobanteVenta

---
+++ facturacion-electronica-app/src/main/java/com/wallparisoft/facturacion/modelo/ComprobanteVenta.java
@@ -0,0 +1,35 @@
+package com.wallparisoft.facturacion.modelo;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Entity
+@Table(name = "comprobante_venta")
+@Data
+public class ComprobanteVenta {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id_comprobante_venta")
+	private Integer idComprobanteVenta;
+
+	@Column(name = "tipo_comprobante", nullable = false, length = 50)
+	private String tipoComprobante;
+
+	@Column(name = "fecha_generacion")
+	private LocalDateTime fechaGeneracion;
+
+	@PrePersist
+	public void preInsertar() {
+		fechaGeneracion = LocalDateTime.now();
+	}
+
+}
