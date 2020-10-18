a911c0fedb39fe33e9f1b0df4ebac16e65f62592
jeddiewars <jguerreroh@outlook.com>
2020-10-18 10:57:34 -0500

    cambio nombre proy

--- facturacion-electronica/src/main/java/com/facturacion/electronica/repo/IComprobanteVentaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.facturacion.electronica.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.facturacion.electronica.modelo.ComprobanteVenta;
-
-public interface IComprobanteVentaRepo extends JpaRepository<ComprobanteVenta, Integer> {
-
-}
--- facturacion-electronica/src/main/java/com/facturacion/electronica/servicio/IComprobanteVentaServicio.java
+++
@@ -1,7 +0,0 @@
-package com.facturacion.electronica.servicio;
-
-import com.facturacion.electronica.modelo.ComprobanteVenta;
-
-public interface IComprobanteVentaServicio extends ICRUD<ComprobanteVenta> {
-
-}
