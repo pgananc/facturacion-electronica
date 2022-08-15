d0b4c04e0fc243eda3bb22f05b21c279cdd40060
Jonathan Guerrero <jguerreroh@outlook.com>
2022-08-15 08:33:13 -0500

    java version change, spring version change

--- ebill-customer/src/main/java/com/wallparisoft/ebill/customer/EBillCustomerApplication.java
+++ ebill-customer/src/main/java/com/wallparisoft/ebill/customer/EBillCustomerApplication.java
@@ -1,4 +1,4 @@
-package com.wallparisoft;
+package com.wallparisoft.ebill.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
