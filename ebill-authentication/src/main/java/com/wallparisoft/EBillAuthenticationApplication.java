5350a81729585bd1bd8f4555e0d712f4b1f5bdbd
Jonathan Guerrero <jguerreroh@outlook.com>
2022-08-17 07:55:35 -0500

    db connection

--- ebill-authentication/src/main/java/com/wallparisoft/EBillAuthenticationApplication.java
+++ ebill-authentication/src/main/java/com/wallparisoft/EBillAuthenticationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EBillAuthenticationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoginAuthApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(EBillAuthenticationApplication.class, args);
+  }
 
 }
